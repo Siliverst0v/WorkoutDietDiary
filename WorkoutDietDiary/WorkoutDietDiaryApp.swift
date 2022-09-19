ba8a12734939ceba726e89aadd4b53207c31bed5
Siliverst0v <94259002+Siliverst0v@users.noreply.github.com>
2022-09-19 19:12:35 +0300

    Initial Commit

---
+++ WorkoutDietDiary/WorkoutDietDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutDietDiaryApp.swift
+//  WorkoutDietDiary
+//
+//  Created by Анатолий Силиверстов on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutDietDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
